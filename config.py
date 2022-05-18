8b0dc0d47f2f45646a72f5706b8503e03d69f44f
Roman <100369920+danilov-ra83@users.noreply.github.com>
2022-05-18 22:33:14 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = '5360341778:AAGFd0omH88YTBEhjN7woJ6PNiIY3_-6osI'
+keys = {
+    'юань': 'CNY',
+    'йена': 'JPY',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'франк': 'CHF',
+    'лира': 'TRY',
+    'рубль': 'RUB',
+}
